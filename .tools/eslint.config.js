f8c4c3a3889914d76a4385e1a6e7e192d16931c5
Tunkasina <klodonline@gmail.com>
2025-07-04 07:36:46 +0200

    fix: linter too long on www/js by excluding www/js/inc

--- .tools/eslint.config.js
+++ .tools/eslint.config.js
@@ -4,6 +4,7 @@ import pluginPrettier from "eslint-plugin-prettier";
 export default [
   {
     files: ["**/*.js"],
+    ignores: ["project/www/js/inc/**"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module"
