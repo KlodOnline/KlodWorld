520f4ca4da9509969b1aaa562084079832e8f406
Tunkasina <klodonline@gmail.com>
2025-07-08 21:03:04 +0200

    refactor: eslint config files

--- .tools/eslint.config.js
+++ .tools/eslint.config.js
@@ -1,21 +1,24 @@
-import prettier from "eslint-config-prettier";
-import pluginPrettier from "eslint-plugin-prettier";
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
   {
-    files: ["**/*.js"],
-    ignores: ["project/www/js/inc/**"],
+    files: ['**/*.js'],
+    ignores: ['project/www/js/inc/**'],
+    ignorePatterns: [
+      ".tools/**",
+      "www/js/inc/**"
+    ],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
+      ecmaVersion: 'latest',
+      sourceType: 'module'
     },
     plugins: {
       prettier: pluginPrettier
     },
     rules: {
-      "prettier/prettier": "error"
+      'prettier/prettier': 'error'
     }
   },
   prettier
 ];
-
